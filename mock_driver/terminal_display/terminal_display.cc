8277f8d08dd4384c8f5887eaa45b9a19ae740657
Jayce <2512077898@qq.com>
2026-06-23 16:07:53 +0800

    feat(app): simulated sensing and status display loop

--- mock_driver/terminal_display/terminal_display.cc
+++ mock_driver/terminal_display/terminal_display.cc
@@ -7,7 +7,7 @@
 
 namespace coffee_keeper {
 namespace {
-const char* ToString(ProcessMode mode) {
+const char *ToString(ProcessMode mode) {
     switch (mode) {
         case ProcessMode::kDry:
             return "Dry";
@@ -17,7 +17,7 @@ const char* ToString(ProcessMode mode) {
     return "Unknown";
 }
 
-const char* ToString(SystemStatus state) {
+const char *ToString(SystemStatus state) {
     switch (state) {
         case SystemStatus::kChecking:
             return "Checking";
@@ -34,16 +34,29 @@ const char* ToString(SystemStatus state) {
     }
     return "Unknown";
 }
+
+const char *ToString(CoffeeStatus coffee_status) {
+    switch (coffee_status) {
+        case CoffeeStatus::kTooDry:
+            return "TooDry";
+        case CoffeeStatus::kTooDamp:
+            return "TooDamp";
+        case CoffeeStatus::kNormal:
+            return "Normal";
+    }
+    return "Unknown";
+}
 }  // namespace
-void TerminalDisplay::Show(const DisplayData& data) {
+void TerminalDisplay::Show(const DisplayData &data) {
     std::cout << "Temperature: " << data.reading.temperature << " C\n"
               << "Humidity: " << data.reading.humidity << " %\n"
               << "Mode: " << ToString(data.mode) << "\n"
+              << "Coffee Status: " << ToString(data.coffee_status) << "\n"
               << "State: " << ToString(data.state) << "\n"
               << std::endl;
 }
 
-void TerminalDisplay::ShowError(const std::string& message) {
+void TerminalDisplay::ShowError(const std::string &message) {
     std::cerr << "[Error] " << message << std::endl;
 }
 }  // namespace coffee_keeper
